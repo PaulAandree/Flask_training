7291f92453601c4665862a744d619d51b78964ab
PaulAandree <defthldre@outlook.com>
2023-05-17 21:58:07 -0500

    towns concessions missing

--- prueba_flask_versiones.py
+++ prueba_flask_versiones.py
@@ -347,4 +347,178 @@ combined_chart = alt.hconcat(chart, bar)
 st.altair_chart(combined_chart, use_container_width=True)
 
 st.write(g_dist[g_dist["PROVINCIA"]== "ABANCAY"])
------------------------------------------------------------------------------------
+#
+# ---------------------------------LEAN STUFF-------------------------------------
+
+import streamlit as st
+import altair as alt
+import pandas as pd
+
+# Define the data
+data = pd.read_csv(r"E:\\_02_practicas_region_apurimac\\Tareas_sub gerente(David )\\Tarea_03 - indicadores\\Apurimac.csv")
+
+g_prov = data.groupby("PROVINCIA").sum().reset_index()
+g_dist = data.groupby(["PROVINCIA", "DISTRITO"]).sum().reset_index()
+
+a_p = round(g_prov["VIVIENDAS CON LUZ"] / (g_prov["VIVIENDAS CON LUZ"] + g_prov["VIVIENDAS SIN LUZ"]) * 100, 2)
+b_p = 100 - a_p
+
+a_d = round(g_dist["VIVIENDAS CON LUZ"] / (g_dist["VIVIENDAS CON LUZ"] + g_dist["VIVIENDAS SIN LUZ"]) * 100, 2)
+b_d = 100 - a_d
+
+g_prov["% VIVIENDAS CON ACCESO"]   = a_p
+g_prov["% VIVIENDAS SIN ACCESO"]   = b_p
+g_dist["% VIVIENDAS CON ACCESO"] = a_d
+g_dist["% VIVIENDAS SIN ACCESO"] = b_d
+
+# Sort the data by "VIVIENDAS CON LUZ %" for province chart
+g_prov = g_prov.sort_values("% VIVIENDAS CON ACCESO")
+
+# Create the main chart using Altair
+chart = (
+    alt.Chart(g_prov)
+    .transform_fold(
+        ["% VIVIENDAS CON ACCESO", "% VIVIENDAS SIN ACCESO"],
+        as_=["Estado de acceso", "Porcentaje"],
+    )
+    .mark_bar()
+    .encode(
+        y=alt.Y("PROVINCIA:N", sort="-x"),
+        x=alt.X("Porcentaje:Q", stack="normalize", axis=alt.Axis(format=".2%")),
+        color=alt.Color(
+            "Estado de acceso:N",
+            legend=alt.Legend(title="Estado de acceso"),
+            scale=alt.Scale(
+                domain=["% VIVIENDAS CON ACCESO", "% VIVIENDAS SIN ACCESO"],
+                range=["#50B4C7", "#ED7D31"],
+            ),
+        ),
+        order=alt.Order("Estado de acceso:N"),
+    )
+    .properties(
+        title="BRECHAS A CUBRIR EN FLUIDO ELECTRICO POR PROVINCIA EN APURIMAC",
+        width=600,
+        height=400,
+    )
+)
+
+# Show the main chart
+st.altair_chart(chart, use_container_width=True)
+
+# Sort the data by "VIVIENDAS CON LUZ %_d" for district chart
+g_dist = g_dist.sort_values("% VIVIENDAS CON ACCESO")
+
+# Add a subheader and display the selected province's district bar chart
+selected_province = st.selectbox("Seleccione una provincia", g_prov["PROVINCIA"])
+selected_province_data = g_dist[g_dist["PROVINCIA"] == selected_province]
+
+st.subheader(f"Brecha electrica a cubrir en {selected_province}")
+chart_data = pd.melt(selected_province_data, id_vars=["DISTRITO"], value_vars=["% VIVIENDAS CON ACCESO", "% VIVIENDAS SIN ACCESO"], var_name="Estado de acceso", value_name="Porcentaje")
+chart_data["Porcentaje"] = chart_data["Porcentaje"].astype(float) / 100.0
+bar_chart = alt.Chart(chart_data).mark_bar().encode(
+    x=alt.X("Porcentaje:Q", stack="normalize", axis=alt.Axis(format=".2%")),
+    y=alt.Y("DISTRITO:N", sort="-x"),
+    color=alt.Color(
+        "Estado de acceso:N",
+        legend=alt.Legend(title="Estado de acceso"),
+        scale=alt.Scale(
+            domain=["% VIVIENDAS CON ACCESO", "% VIVIENDAS SIN ACCESO"],
+            range=["#50B4C7", "#ED7D31"],
+        ),
+    ),
+    order=alt.Order("Estado de acceso:N")
+).properties(width=600, height=400)
+
+st.altair_chart(bar_chart, use_container_width=True)
+
+
+# ---------------------------------BUTTON[CHUNKY STUFF].SAVE AS PNG-------------------------------------
+# ---------------------almost there------------------------
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Define the data
+data = pd.read_csv(r"E:\\_02_practicas_region_apurimac\\Tareas_sub gerente(David )\\Tarea_03 - indicadores\\Apurimac.csv")
+
+g_prov = data.groupby("PROVINCIA").sum().reset_index()
+g_dist = data.groupby(["PROVINCIA", "DISTRITO"]).sum().reset_index()
+
+a_p = round(g_prov["VIVIENDAS CON LUZ"] / (g_prov["VIVIENDAS CON LUZ"] + g_prov["VIVIENDAS SIN LUZ"]) * 100, 2)
+b_p = 100 - a_p
+
+a_d = round(g_dist["VIVIENDAS CON LUZ"] / (g_dist["VIVIENDAS CON LUZ"] + g_dist["VIVIENDAS SIN LUZ"]) * 100, 2)
+b_d = 100 - a_d
+
+g_prov["% VIVIENDAS CON ACCESO"] = a_p.astype(str)
+g_prov["% VIVIENDAS SIN ACCESO"] = b_p.astype(str)
+g_dist["% VIVIENDAS CON ACCESO"] = a_d.astype(str)
+g_dist["% VIVIENDAS SIN ACCESO"] = b_d.astype(str)
+
+# Sort the data by "% VIVIENDAS CON ACCESO" for province chart
+g_prov = g_prov.sort_values("% VIVIENDAS CON ACCESO")
+
+
+## option that saves this charts as .png files
+# Create the main province bar chart
+fig, ax = plt.subplots(figsize=(7.5,6))
+province_bars = ax.barh(g_prov["PROVINCIA"], g_prov["% VIVIENDAS CON ACCESO"].astype(float),
+                        color="#50B4C7", label="% VIVIENDAS CON ACCESO")
+ax.barh(g_prov["PROVINCIA"], g_prov["% VIVIENDAS SIN ACCESO"].astype(float),
+        left=g_prov["% VIVIENDAS CON ACCESO"].astype(float), color="#ED7D31", label="% VIVIENDAS SIN ACCESO")
+
+# Add percentage labels to the province bars
+for (bar_con, bar_sin) in zip(province_bars, ax.patches[len(province_bars):]):
+    width_con = bar_con.get_width()
+    width_sin = bar_sin.get_width() +100
+    x_con = width_con - 35
+    x_sin = width_sin
+    y_con = bar_con.get_y() + bar_con.get_height() / 2
+    y_sin = bar_sin.get_y() + bar_sin.get_height() / 2
+    ax.text(x_con, y_con, f"{width_con:.1f}%", ha="center", va="center")
+    ax.text(x_sin, y_sin, f"{width_sin:.1f}%", ha="center", va="center")
+
+st.write(g_prov["% VIVIENDAS CON ACCESO"])
+
+# Configure the chart appearance
+ax.set_xlabel("Porcentaje")
+ax.set_ylabel("Provincia")
+ax.set_title("BRECHAS A CUBRIR EN FLUIDO ELECTRICO POR PROVINCIA EN APURIMAC")
+ax.legend()
+plt.legend(loc='upper left', bbox_to_anchor=(1.05, 1))
+
+# Display the province bar chart
+st.pyplot(fig)
+
+# Add a subheader and display the selected province's district bar chart
+selected_province = st.selectbox("Select a province", g_prov["PROVINCIA"])
+selected_province_data = g_dist[g_dist["PROVINCIA"] == selected_province].copy()
+selected_province_data = selected_province_data.sort_values("% VIVIENDAS CON ACCESO")
+
+# Create the district bar chart
+fig, ax = plt.subplots()
+district_bars = ax.barh(selected_province_data["DISTRITO"], selected_province_data["% VIVIENDAS CON ACCESO"].astype(float),
+                        color="#50B4C7", label="% VIVIENDAS CON ACCESO")
+ax.barh(selected_province_data["DISTRITO"], selected_province_data["% VIVIENDAS SIN ACCESO"].astype(float),
+        left=selected_province_data["% VIVIENDAS CON ACCESO"].astype(float), color="#ED7D31", label="% VIVIENDAS SIN ACCESO")
+
+# Add percentage labels to the district bars
+for bar_con, bar_sin in zip(district_bars, ax.patches[len(district_bars):]):
+    width_con = bar_con.get_width()
+    width_sin = bar_sin.get_width()
+    x_con = width_con + 1
+    x_sin = width_sin + 1
+    y_con = bar_con.get_y() + bar_con.get_height() / 2
+    y_sin = bar_sin.get_y() + bar_sin.get_height() / 2
+    ax.text(x_con, y_con, f"{width_con:.1f}%", ha="center", va="center")
+    ax.text(x_sin, y_sin, f"{width_sin:.1f}%", ha="center", va="center")
+
+# Configure the chart appearance
+ax.set_xlabel("Porcentaje")
+ax.set_ylabel("Distrito")
+ax.set_title(f"BRECHAS A CUBRIR EN FLUIDO ELECTRICO EN DISTRITOS DE {selected_province}")
+ax.legend()
+plt.legend(loc='upper left', bbox_to_anchor=(1.05, 1))
+
+# Display the district bar chart
+st.pyplot(fig)
